bcf1302827b077dd9bf3c7fbc2b47d909c1d9a86
Sarthak S Kumar <sskworld9742@gmail.com>
2022-02-27 09:32:29 +0530

    Major Changes

--- Code/Rat in a Maze/Rat_in_a_maze (With UI).py
+++ Code/Rat in a Maze/Rat_in_a_maze (With UI).py
@@ -1,12 +1,20 @@
 '''
 #Project Name: Rat in a maze
-#Last Updated: 24/02/2022 15:05
+#Last Updated: 27/02/2022 02:17
 #Last Updated by: Sarthak S Kumar
-
 #Changelog:
+`   27/02/2022 02:17 Sarthak S Kumar
+        # Fixed Overlapping Text when user solves the maze
+        # Widget Scaling Corrections
+        # Fixed Background Images descaling and moving out of frame
+
+    27/02/2022 22:59 Varun Chandrashekar
+        # Function to manage maze box sizes and other fixes
+        # Fixed obstacle boxes appearing after backtracking solution
+
     24/02/2022 15:05 Sarthak S Kumar
         # Success and Failure Messages
-        
+
     24/02/2022 15:10 Varun Chandrashekar
         # Scaling Issues resolved
 
@@ -47,37 +55,79 @@ import copy
 import datetime
 from tkinter import *
 from tkinter import ttk
-from ctypes import windll
 from PIL import Image
 from PIL import ImageTk
+
+# To Enable High Resolution Scaling in Windows OS
+
+from ctypes import windll
 windll.shcore.SetProcessDpiAwareness(1)
 
+global first_puzzle
+first_puzzle = True
 
-def main():
+# Set the size of the box according to the screen resolution and the number of boxes
+
+
+def manage_box_size(height, width, N):
+    size = int(((height*width)/(100*N**2)))
+    if size > 50:
+        return manage_box_size(height, width, 1.5*N)
+    return size
+
+
+def main():  # Program execution begins from here.
+    """Tkinter Window Initialisation"""
+    master = Tk()
+    master.title("Rat in a Maze")
+    # master.attributes('-fullscreen', True) #To enable fullscreen mode
+
+    # Get the resolution of working monitor
+    height = master.winfo_screenheight()
+    width = master.winfo_screenwidth()
+    resolution = str(width) + "x" + str(height)
+
+    master.geometry(resolution)
+    master.configure(bg="#ffffff")
+
+    if first_puzzle:
+
+        """ Welcome Screen """
+        intro = Frame(master)
+        intro.place(anchor="nw")
+
+        bg = PhotoImage(file=r"Code\Rat in a Maze\Assets\intro.png")  # ./Assets/intro.png (Mac)
+        canvas1 = Canvas(intro, height=height, width=width)
+        canvas1.pack()
+        canvas1.create_image(0, 0, image=bg, anchor="nw")
+
+        intro.after(3000, intro.destroy)
+        intro.wait_window(intro)
+
     """User Entry Screen"""
     user_entry = Frame(master, background="#ffffff")
     user_entry.pack()
 
-    bg = PhotoImage(file=r"Code\Rat in a Maze\Assets\user_entry.png")
-    canvas1 = Canvas(user_entry, height=1080, width=2000)
+    bg = PhotoImage(file=r"Code\Rat in a Maze\Assets\user_entry.png")  # ./Assets/user_entry.png (Mac)
+    canvas1 = Canvas(user_entry, height=height, width=width)
     canvas1.pack()
     canvas1.create_image(0, 0, image=bg, anchor="nw")
 
-    Label(user_entry, text="Welcome", font=(r"HK Grotesk", 50), fg="#000000", bg="#ffffff").place(anchor='c', x=960, y=275)
+    Label(user_entry, text="Welcome", font=(r"HK Grotesk", 50), fg="#000000", bg="#ffffff").place(anchor='c', x=(960/1920)*width + (2*width)/10, y=(275/1080)*height)
 
-    Label(user_entry, text="Your Name: ", font=(r"HK Grotesk", 25), fg="#000000", bg="#ffffff").place(anchor='e', x=800, y=450)
+    Label(user_entry, text="Your Name: ", font=(r"HK Grotesk", 25), fg="#000000", bg="#ffffff").place(anchor='e', x=(800/1920)*width + (2*width)/10, y=(450/1080)*height)
 
     sizebox = Entry(user_entry, font=(r"HK Grotesk", 20), fg="#000000", bg="#ffffff")
-    sizebox.place(anchor='w', x=960, y=450)
+    sizebox.place(anchor='w', x=(960/1920)*width + (2*width)/10, y=(450/1080)*height)
 
-    Label(user_entry, text="Maze Size: ", font=(r"HK Grotesk", 25), fg="#000000", bg="#ffffff").place(anchor='e', x=800, y=540)
+    Label(user_entry, text="Maze Size: ", font=(r"HK Grotesk", 25), fg="#000000", bg="#ffffff").place(anchor='e', x=(800/1920)*width + (2*width)/10, y=(540/1080)*height)
 
     clicked = StringVar()
     clicked.set("Select one")
 
     gridsizes = ["5 x 5", "8 x 8", "10 x 10", "15 x 15", "20 x 20", "25 x 25"]
     drop = OptionMenu(user_entry, clicked, *gridsizes)
-    drop.place(anchor='w', x=960, y=540)
+    drop.place(anchor='w', x=(960/1920)*width + (2*width)/10, y=(540/1080)*height)
     drop.config(font=(r'HK Grotesk', (20)), bg='#ffffff', fg="#000000")
 
     def movenext():
@@ -87,7 +137,7 @@ def main():
         N = int(x[0])
         user_entry.after(1000, user_entry.destroy)
 
-    submit = Button(user_entry, text="Submit", command=movenext, bg="#4d1354", font=(r'HK Grotesk', (25)), fg="white").place(anchor='c', x=960, y=800)
+    submit = Button(user_entry, text="Submit", command=movenext, bg="#4d1354", font=(r'HK Grotesk', (25)), fg="white").place(anchor='c', x=(960/1920)*width + (2*width)/10, y=(800/1920)*width)
 
     user_entry.wait_window(user_entry)
 
@@ -133,7 +183,7 @@ def main():
         break
 
     """Maze UI"""
-    squaresize = int(60 / N + 30)  # Dynamically changes with the maze size
+    squaresize = manage_box_size(height, width, N)  # Dynamically changes with the maze size
     tile_color = " "
     rectbox_coordinates = [0, 0, squaresize, squaresize]
     forbidden = []  # To store co-ordinates of obstacle boxes in maze grid
@@ -141,17 +191,17 @@ def main():
     maze_UI = Frame(master, background="#fffceb")
     maze_UI.pack()
 
-    bg2 = PhotoImage(file=r"Code\Rat in a Maze\Assets\maze_ui.png")
-    canvas2 = Canvas(maze_UI, height=1080, width=2000)
+    bg2 = PhotoImage(file=r"Code\Rat in a Maze\Assets\maze_ui.png")  # ./Assets/maze_ui.png (Mac)
+    canvas2 = Canvas(maze_UI, height=height, width=width)
     canvas2.pack()
     canvas2.create_image(0, 0, image=bg2, anchor="nw")
 
     headline = Label(maze_UI, text=f"Hello {username}\n Your {tile_color.join(x)} maze is here. Solve it", font=(r"HK Grotesk", 30), fg="#000000", bg="#ffffff")
-    headline.place(anchor='center', x=1390, y=375)
+    headline.place(anchor='center', x=(1390/1920)*width, y=(375/1080)*height)
 
     # Canvas to display Maze to be solved
-    question_canvas = Canvas(maze_UI, height=(N * squaresize + (N)), width=(N * squaresize + (N)), bg='#ffffff', bd=0, highlightthickness=0, relief='ridge')
-    question_canvas.place(anchor='center', x=500, y=512)
+    question_canvas = Canvas(maze_UI, height=(N * squaresize)-squaresize/10, width=(N * squaresize)-squaresize/10, bg='#ffffff')
+    question_canvas.place(anchor='center', x=(500/1920)*width, y=(512/1080)*height)
 
     # Drawing the maze to be solved in the question canvas
     for i in maze:
@@ -162,7 +212,7 @@ def main():
                 color = "black"
                 forbidden.append((rectbox_coordinates[0], rectbox_coordinates[1], rectbox_coordinates[2], rectbox_coordinates[3]))
 
-            square = question_canvas.create_rectangle(rectbox_coordinates[0], rectbox_coordinates[1], rectbox_coordinates[2], rectbox_coordinates[3], fill=color, width=1)
+            square = question_canvas.create_rectangle(rectbox_coordinates[0], rectbox_coordinates[1], rectbox_coordinates[2], rectbox_coordinates[3], fill=color, width=0)
             rectbox_coordinates[0] += squaresize
             rectbox_coordinates[2] += squaresize
 
@@ -176,17 +226,18 @@ def main():
     endpoint = question_canvas.create_rectangle(squaresize*(N-1), squaresize*(N-1), squaresize*N, squaresize * N, fill="#660033")
 
     use = Label(maze_UI, text="Use", font=(r"HK Grotesk", 20), fg="#000000", bg="#ffffff")
-    use.place(anchor='e', x=1235, y=540)
+    use.place(anchor='e', x=((height+width)/2 - (width/7)), y=(540/1080)*height)
 
-    img = Image.open(r"Code\Rat in a Maze\Assets\arrowkeys.png")
-    img = img.resize((100, 100), Image.ANTIALIAS)
+    img = Image.open(r"Code\Rat in a Maze\Assets\arrowkeys.png")  # ./Assets/arrowkeys.png (Mac)
+    img = img.resize((int((height*width)/(4*(height+width))), int((height*width)/(4*(height+width)))), Image.ANTIALIAS)
     photoImg = ImageTk.PhotoImage(img)
-    arrow = Canvas(maze_UI, width=100, height=100, bg="white", bd=0, highlightthickness=0, relief='ridge')
-    arrow.place(x=1360, y=540, anchor='e')
+
+    arrow = Canvas(maze_UI, width=(height*width)/(4*(height+width)), height=(height*width)/(4*(height+width)), bg="white", bd=0, highlightthickness=0, relief='ridge')
+    arrow.place(x=(height+width)/2 - (width/22), y=(540/1080)*height, anchor='e')
     arrow.create_image(0, 0, image=photoImg, anchor='nw')
 
     labe = Label(maze_UI, text="to move the rat", font=(r"HK Grotesk", 20), fg="#000000", bg="#ffffff")
-    labe.place(anchor='w', x=1385, y=540)
+    labe.place(anchor='w', x=(height+width)/2 - (width/25), y=(540/1080)*height)
 
     visited_squares = []  # List to stores the squares in which the rat has been before
 
@@ -201,22 +252,24 @@ def main():
 
     def nextstep():  # When Next button is clicked
         Next = Tk()
-        Next.geometry("1500x300")
+        Next.geometry(str(int(1500/1920*width)) + "x" + str(int(300/1080*height)))
         Next.title("Solving the Maze")
         Next.configure(bg="#ffffff", border=1)
         message = Label(Next, text=f"Wanna solve another maze?", font=(
             r"HK Grotesk", 30), fg="#000000", bg="#ffffff")
-        message.place(anchor='center', x=750, y=100)
+        message.place(anchor='center', x=(750/1080)*height, y=(100/1080)*height)
 
         def restart():  # Restart the program
+            global first_puzzle
             Next.destroy()
-            maze_UI.destroy()
+            master.destroy()
+            first_puzzle = False
             main()
 
         yes = Button(Next, text="Yeah", command=restart, bg="#4d1354", font=(r'HK Grotesk', (20)), fg="white")
-        yes.place(anchor='center', x=550, y=200)
+        yes.place(anchor='center', x=(550/1080)*height, y=(200/1080)*height)
         no = Button(Next, text="Nah", command=exit, bg="#4d1354", font=(r'HK Grotesk', (20)), fg="white")
-        no.place(anchor='center', x=950, y=200)
+        no.place(anchor='center', x=(950/1080)*height, y=(200/1080)*height)
 
     def user_solved():  # When user manages to solve the maze
 
@@ -230,15 +283,15 @@ def main():
         endtime = datetime.datetime.now()
         td = endtime - starttime
         elapsed = td.total_seconds()
-
         successmessages = ["Yay! You made it!", "Good one fella!", "That was quite easy!", "Wonderful!", "You aced it!"]
-        Label(maze_UI, text="🎉 Congratulations 🎉", font=(r"HK Grotesk", 40), fg="#000000", bg="#ffffff").place(anchor='e', x=1720, y=375)
-        Label(maze_UI, text=random.choice(successmessages), font=(r"HK Grotesk", 30), fg="#000000", bg="#ffffff").place(anchor='center', x=1390, y=455)
-        Label(maze_UI, text=f"You solved the maze in {round(elapsed, 2)} seconds", font=(r"HK Grotesk", 20), fg="#000000", bg="#ffffff").place(anchor='center', x=1390, y=520)
+        Label(maze_UI, text="🎉 Congratulations 🎉", font=(r"HK Grotesk", 40), fg="#000000", bg="#ffffff").place(anchor='center', x=(1390/1920)*width, y=(375/1080)*height)
+        Label(maze_UI, text=random.choice(successmessages), font=(r"HK Grotesk", 30), fg="#000000", bg="#ffffff").place(anchor='center', x=(1390/1920)*width, y=(455/1080)*height)
+        Label(maze_UI, text=f"You solved the maze in {round(elapsed, 2)} seconds", font=(r"HK Grotesk", 20), fg="#000000", bg="#ffffff").place(anchor='center', x=(1390/1920)*width, y=(520/1080)*height)
 
         # Displays Confirmation Window on clicking next
-        nextb = Button(maze_UI, text="Next", command=nextstep, bg="#4d1354", font=(r'HK Grotesk', (20)), fg="white")
-        nextb.place(anchor='center', x=1390, y=600)
+        nextb = Button(maze_UI, text="Next", command=nextstep,
+                       bg="#4d1354", font=(r'HK Grotesk', (20)), fg="white")
+        nextb.place(anchor='center', x=(1390/1920)*width, y=(600/1080)*height)
 
     # Keybind Events
     def left(event):
@@ -290,15 +343,16 @@ def main():
     master.bind("<Up>", up)
     master.bind("<Down>", down)
 
-    global starttime, endtime  # To start the timer
+    global starttime, endtime
     starttime = datetime.datetime.now()
 
-    def solve():  # When user gives up on solving the maze
+    # When user gives up on solving the maze
+    def solve():
         question_canvas.destroy()
 
         # To display the maze solution
-        solution_canvas = Canvas(maze_UI, height=(N * squaresize + (N)), width=(N * squaresize + (N)), bg='#ffffff')
-        solution_canvas.place(anchor='center', x=500, y=512)
+        solution_canvas = Canvas(maze_UI, height=(N * squaresize)-squaresize/10, width=(N * squaresize)-squaresize/10, bg='#ffffff')
+        solution_canvas.place(anchor='center', x=(500/1920)*width, y=(512/1080)*height)
 
         rectbox_coordinates = [0, 0, squaresize, squaresize]
         for i in sol:
@@ -310,7 +364,7 @@ def main():
                 else:
                     color = "black"
                 square = solution_canvas.create_rectangle(
-                    rectbox_coordinates[0], rectbox_coordinates[1], rectbox_coordinates[2], rectbox_coordinates[3], fill=color, width=1)
+                    rectbox_coordinates[0], rectbox_coordinates[1], rectbox_coordinates[2], rectbox_coordinates[3], fill=color, width=0)
                 rectbox_coordinates[0] += squaresize
                 rectbox_coordinates[2] += squaresize
 
@@ -324,37 +378,22 @@ def main():
         headline.destroy()
         comp_solve.destroy()
 
-        failmessages = ["You gave up so quick!", "Was it really tough?", "Help yourself!", "You couldn't make it!"]
-        Label(maze_UI, text=random.choice(failmessages), font=(r"HK Grotesk", 40), fg="#000000", bg="#ffffff").place(anchor='center', x=1390, y=375)
-        Label(maze_UI, text=f"Better luck next time!", font=(r"HK Grotesk", 20), fg="#000000", bg="#ffffff").place(anchor='center', x=1390, y=475)
+        failmessages = [r"❌ You gave up so quick!", r"🧠 Was it really tough?", r"😂 Help yourself!", "👎 You couldn't make it!"]
+        Label(maze_UI, text=random.choice(failmessages), font=(r"HK Grotesk", 40), fg="#000000", bg="#ffffff").place(anchor='center', x=(1390/1920)*width, y=(375/1080)*height)
+        Label(maze_UI, text=f"Better luck next time!", font=(r"HK Grotesk", 20), fg="#000000", bg="#ffffff").place(anchor='center', x=(1390/1920)*width, y=(475/1080)*height)
 
         # Displays Confirmation Window on clicking next
-        nextb = Button(maze_UI, text="Next", command=nextstep, bg="#4d1354", font=(r'HK Grotesk', (20)), fg="white")
-        nextb.place(anchor='center', x=1390, y=600)
+        nextb = Button(maze_UI, text="Next", command=nextstep,
+                       bg="#4d1354", font=(r'HK Grotesk', (20)), fg="white")
+        nextb.place(anchor='center', x=(1390/1920)*width, y=(600/1080)*height)
 
     # Button to let computer display the solution
-    comp_solve = Button(maze_UI, text="Give Up!", command=solve, bg="#4d1354", font=(r'HK Grotesk', (20)), fg="white")
-    comp_solve.place(anchor='center', x=1390, y=800)
+    comp_solve = Button(maze_UI, text="Give Up!", command=solve,
+                        bg="#4d1354", font=(r'HK Grotesk', (20)), fg="white")
+    comp_solve.place(anchor='center', x=(1390/1920)*width, y=(800/1080)*height)
 
     mainloop()
 
 
-if __name__ == "__main__":  # Program Execution begins from here.
-    """Tkinter Window Initialisation"""
-    master = Tk()
-    master.title("Rat in a Maze")
-    master.geometry("1900x1000")
-    master.configure(bg="#ffffff")
-
-    """ Welcome Screen """
-    intro = Frame(master)
-    intro.place(anchor="nw")
-
-    bg = PhotoImage(file=r"Code\Rat in a Maze\Assets\intro.png")
-    canvas1 = Canvas(intro, height=1080, width=2000)
-    canvas1.pack()
-    canvas1.create_image(0, 0, image=bg, anchor="nw")
-
-    intro.after(3000, intro.destroy)
-    intro.wait_window(intro)
+if __name__ == "__main__":
     main()
